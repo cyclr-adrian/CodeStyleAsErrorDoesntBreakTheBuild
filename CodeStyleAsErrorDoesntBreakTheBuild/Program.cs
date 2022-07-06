fe9d2ca434423225bc785c4e5db7ebc2cbdfebf4
Adrian Wright <adrian.wright@cyclr.com>
2022-07-06 10:20:07 +0100

    Add project files.

---
+++ CodeStyleAsErrorDoesntBreakTheBuild/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeStyleAsErrorDoesntBreakTheBuild
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 0; i < args.Length; i++)
+            {
+
+            }
+
+            if (i < 5)
+                throw new Exception("");
+        }
+    }
+}
